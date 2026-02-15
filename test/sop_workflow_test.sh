d1ab96cdea3aff5e1b25539e718774debe7ef600
qtpi-bonding <qtpi-bonding@noreply.email.com>
2026-02-14 16:07:24 -0800

    legal copyright update on files

--- test/sop_workflow_test.sh
+++ test/sop_workflow_test.sh
@@ -1,3 +1,21 @@
+# PocketCoder: An accessible, secure, and user-friendly open-source coding assistant platform.
+# Copyright (C) 2026 Qtpi Bonding LLC
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+# @pocketcoder-core: Governance Test. Ensures the full cycle from Proposal to Materialized Skill is secure.
 #!/bin/bash
 
 # SOP Governance Integration Test (Master of Signature Flow)
