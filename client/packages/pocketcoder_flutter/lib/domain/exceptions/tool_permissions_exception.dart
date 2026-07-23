7e5b57ad277e3a0d2227116505ddf92514c49f8e
qtpi-bonding <qtpi-bonding@noreply.email.com>
2026-07-23 05:14:51 -0700

    feat(tool-permissions): add ToolPermissionDao/ToolPermissionRepository, remove dead duplicate exception

--- client/packages/pocketcoder_flutter/lib/domain/exceptions/tool_permissions_exception.dart
+++
@@ -1,9 +0,0 @@
-class ToolPermissionsException implements Exception {
-  final String message;
-  final Object? cause;
-
-  ToolPermissionsException(this.message, {this.cause});
-
-  @override
-  String toString() => 'ToolPermissionsException: $message';
-}
