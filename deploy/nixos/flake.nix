397240fade7d3d0390f9b7ac6ea32b45fedbe440
qtpi-bonding <qtpi-bonding@noreply.email.com>
2026-08-06 14:15:21 -0700

    harden NixOS and pin runtime images

--- deploy/nixos/flake.nix
+++ deploy/nixos/flake.nix
@@ -2,7 +2,7 @@
   description = "PocketCoder NixOS server image for Linode";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }: let
