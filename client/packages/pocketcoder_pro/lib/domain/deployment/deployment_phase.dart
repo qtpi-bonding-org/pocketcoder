284e4f914f7eb5d9df5112510d824fd3d67cd9f5
qtpi-bonding <qtpi-bonding@noreply.email.com>
2026-08-07 20:46:47 -0700

    feat(deploy): map wire phases to onboarding stages

--- client/packages/pocketcoder_pro/lib/domain/deployment/deployment_phase.dart
+++ client/packages/pocketcoder_pro/lib/domain/deployment/deployment_phase.dart
@@ -1,3 +1,5 @@
+import 'onboarding_stage.dart';
+
 enum DeploymentPhase {
   waitingForCaddy,
   installingHost,
@@ -10,7 +12,7 @@ enum DeploymentPhase {
   timedOut,
 }
 
-extension DeploymentPhaseWire on DeploymentPhase {
+extension DeploymentPhaseX on DeploymentPhase {
   static DeploymentPhase fromWireName(String? wire) => switch (wire) {
         'installing_host' => DeploymentPhase.installingHost,
         'fetching_release' => DeploymentPhase.fetchingRelease,
@@ -18,5 +20,16 @@ extension DeploymentPhaseWire on DeploymentPhase {
         'compose_up' => DeploymentPhase.composeUp,
         'bootstrap_complete' => DeploymentPhase.bootstrapComplete,
         _ => DeploymentPhase.waitingForCaddy,
+      };
+
+  OnboardingStage toOnboardingStage() => switch (this) {
+        DeploymentPhase.waitingForCaddy => OnboardingStage.securingConnection,
+        DeploymentPhase.installingHost => OnboardingStage.installingHost,
+        DeploymentPhase.fetchingRelease => OnboardingStage.fetchingRelease,
+        DeploymentPhase.loadingImages => OnboardingStage.loadingImages,
+        DeploymentPhase.composeUp => OnboardingStage.startingServices,
+        DeploymentPhase.bootstrapComplete => OnboardingStage.finishingUp,
+        DeploymentPhase.ready => OnboardingStage.ready,
+        DeploymentPhase.failed || DeploymentPhase.timedOut => OnboardingStage.failed,
       };
 }
